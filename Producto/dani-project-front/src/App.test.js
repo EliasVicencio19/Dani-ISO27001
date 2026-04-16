bba13e59bd0a3d4b84c6b20a6a8085a3ef38372a
Jordy <jord.mondaca@duocuc.cl>
2026-04-16 06:20:46 -0400

    Se modifica código de app.test.js por equivocació en la integración del codigo base de DANI

--- Producto/dani-project-front/src/App.test.js
+++ Producto/dani-project-front/src/App.test.js
@@ -1,9 +1,8 @@
-import DaniPlatform from './dani-platform-v3'
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-function App() {
-  return (
-    <DaniPlatform />
-  )
-}
-
-export default App
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
