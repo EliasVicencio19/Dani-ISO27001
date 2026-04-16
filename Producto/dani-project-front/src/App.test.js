cd22db8a57d03533de73d6526896ef6b2ccfc982
Jordy <jord.mondaca@duocuc.cl>
2026-04-16 06:15:05 -0400

    Modificación del codigo base de React a al de dani-platform

--- Producto/dani-project-front/src/App.test.js
+++ Producto/dani-project-front/src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import DaniPlatform from './dani-platform-v3'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+function App() {
+  return (
+    <DaniPlatform />
+  )
+}
+
+export default App
